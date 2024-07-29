66ac5000407bbcc41a67016cd36f8ef4d1903022
Chittagone <chitagobego@gmail.com>
2024-07-30 01:49:23 +0700

    setup dependency fonted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+    });
+  });
   return <div className="bg-red-500">hie</div>;
 };
 
